4439ff0cfcc2eba213a1976d608740b1893f60b8
faizahmed1314 <foyezahmedfj20@gmail.com>
2020-04-06 01:03:14 +0600

    UserLoginModuleAdded

---
+++ EMarketing/Controllers/AdminController.cs
@@ -0,0 +1,46 @@
+﻿using EMarketing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EMarketing.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbemarketingEntities db = new dbemarketingEntities();
+        // GET: Admin
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(tbl_admin admin)
+        {
+            var person = db.tbl_admin.Where(x => x.ad_username == admin.ad_username && x.ad_password == admin.ad_password).SingleOrDefault();
+            if (person != null)
+            {
+                Session["ad_id"] = admin.ad_id.ToString();
+                Session["ad_username"] = admin.ad_username.ToString();
+                return RedirectToAction("Create");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid user name and password!";
+            }
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            if (Session["ad_id"] == null)
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+    }
+}
---
+++ EMarketing/Models/Extended/Admin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EMarketing.Models.Extended
+{
+    [MetadataType(typeof(AdminMetaData))]
+    public partial class tbl_admin
+    {
+
+    }
+    public class AdminMetaData
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "User name is required")]
+        [DataType(DataType.Password)]
+        
+        public string ad_username { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        
+        public string ad_password { get; set; }
+    }
+}
